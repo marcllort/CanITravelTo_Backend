22277596d4db8a8e767854740c5b84aca61cca84
Marc Llort <mac12llm@gmail.com>
2020-06-24 18:14:18 +0200

    Deleted commented AUTOTLS

--- Controller/GetHandler.go
+++ Controller/GetHandler.go
@@ -1,6 +1,9 @@
 package Controller
 
 import (
+	database "CanITravelTo/Database"
+	"CanITravelTo/Model"
+	"CanITravelTo/Utils"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
@@ -11,4 +14,47 @@ func GetHandlerTest(c *gin.Context) {
 
 func GetHandlerTravel(c *gin.Context) {
 	c.String(http.StatusOK, "Only POST requests enabled for this endpoint. Go to canitravelto.com!")
+}
+
+func GetHandler(c *gin.Context) {
+
+	destination := c.DefaultQuery("destination", "Spain")
+	origin := c.DefaultQuery("origin", "_")
+
+	var country Model.InfoCountry
+
+	code := 200
+	allowed := false
+	info := ""
+
+	if Utils.Has(destination) {
+		info = "Destination country exists "
+		// Check borders in the DB --> Return list of countries that can travel there
+		country = database.SelectCountry(db, destination, origin)
+	} else {
+		code = 400
+		allowed = false
+		info = "Destination country does NOT exist "
+	}
+
+	if origin != "_" {
+		if Utils.Has(origin) {
+			info += "- Origin country exists"
+		} else {
+			code = 400
+			allowed = false
+			info += "- Origin country does NOT exist"
+		}
+	}
+
+	c.Header("Access-Control-Allow-Origin", "*")
+	c.Header("Access-Control-Allow-Headers", "access-control-allow-origin, access-control-allow-headers")
+	c.JSON(code, gin.H{
+		"destination": destination,
+		"origin":      origin,
+		"allowed":     allowed,
+		"passport":    country.Info,
+		"info":        info,
+	})
+
 }
--- main.go
+++ main.go
@@ -12,29 +12,15 @@ func main() {
 
 	creds := os.Args[1]
 	router := gin.Default()
-	whitelist := make(map[string]bool)
-	whitelist["localhost"] = true
-	whitelist["canitravelto.com"] = true
-
-	//router.Use(Middleware.IPWhiteList(whitelist))
 
 	// Query string parameters are parsed using the existing underlying request object.
 	// The request responds to a url matching:  /travel?destination=Spain&origin=USA
 	Controller.InitHandler(creds)
 	router.OPTIONS("/travel", Controller.OptionsHandler)
 	router.POST("/travel", Controller.PostHandler)
+	router.GET("/travel", Controller.GetHandler)
 	router.GET("/test", Controller.GetHandlerTest)
-	router.GET("/travel", Controller.GetHandlerTravel)
 
 	router.RunTLS(PORT, creds+"/https-server.crt", creds+"/https-server.key")
-
-	// AutoTLS config
-	/*m := autocert.Manager{
-		Prompt:     autocert.AcceptTOS,
-		HostPolicy: autocert.HostWhitelist("canitravelto.com", "62.57.154.24", "localhost"),
-		Cache:      autocert.DirCache("/var/www/.cache"),
-	}
-
-	log.Fatal(autotls.RunWithManager(router, &m))*/
 
 }
