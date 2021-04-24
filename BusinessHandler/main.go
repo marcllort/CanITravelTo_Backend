cff19b63b3b20c1547f0bce2ca6053d44b6b0a33
marcllort <mac12llm@gmail.com>
2021-04-24 09:33:56 +0200

    Fix Countries with spaces bug

--- BusinessHandler/main.go
+++ BusinessHandler/main.go
@@ -25,7 +25,6 @@ func main() {
 
 	// Query string parameters are parsed using the existing underlying request object.
 	// The request responds to a url matching:  /travel?destination=Spain&origin=USA
-
 	router.OPTIONS("/travel", Controller.OptionsHandler)
 	router.POST("/travel", Controller.PostHandler)
 	router.GET("/travel", Controller.GetHandler)
